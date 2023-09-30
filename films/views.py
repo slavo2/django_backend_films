d47c7c23f5cf52d8b5ac480b16a38f2c8897c735
Slavo <slavo@localhost>
2023-09-30 20:15:07 +0200

    refactor films/views.py

--- films/views.py
+++ films/views.py
@@ -1,12 +1,10 @@
-from django.http import HttpResponse
-from django.template import loader
+from django.shortcuts import render
 from .models import Film
 
 def index(request):
     film_list = Film.objects.all()
 
-    template = loader.get_template("films/index.html")
     context = {
         "film_list": film_list,
     }
-    return HttpResponse(template.render(context, request))
+    return render(request, "films/index.html", context)
