cb977956d10f3cd36eef57f99e14667a8f8644dc
Slavo <slavo@localhost>
2023-10-01 00:43:17 +0200

    Add button to remove filtering by name
    rename GET query parameter filter_name to filter_by_name

--- films/views.py
+++ films/views.py
@@ -2,8 +2,8 @@ from django.shortcuts import render
 from .models import Film
 
 def index(request):
-    if "filter_name" in request.GET:
-        filter_by_name = request.GET["filter_name"]
+    if "filter_by_name" in request.GET:
+        filter_by_name = request.GET["filter_by_name"]
         film_list = Film.objects.filter(name__contains=filter_by_name)
     else:
         filter_by_name = ""
@@ -22,7 +22,7 @@ def index(request):
 
     context = {
         "film_list": film_list,
-        "filter_name": filter_by_name,
+        "filter_by_name": filter_by_name,
         "order_by_name": order_by_name,
     }
     return render(request, "films/index.html", context)
