7b4f36b9cb2f124bf9368ebcd330a58170f10035
Slavo <slavo@localhost>
2023-09-30 23:57:41 +0200

    Add filtering by name functionality
    References:
    https://www.codespeedy.com/detect-if-url-query-parameter-exists-in-django/
    https://docs.djangoproject.com/en/4.2/topics/db/search/

--- films/views.py
+++ films/views.py
@@ -2,9 +2,15 @@ from django.shortcuts import render
 from .models import Film
 
 def index(request):
-    film_list = Film.objects.all()
+    if "filter_name" in request.GET:
+        filter_by_name = request.GET["filter_name"]
+        film_list = Film.objects.filter(name__contains=filter_by_name)
+    else:
+        filter_by_name = ""
+        film_list = Film.objects.all()
 
     context = {
         "film_list": film_list,
+        "filter_name": filter_by_name,
     }
     return render(request, "films/index.html", context)
